c43c5f7f027ff586fcc56292994177fba1ff6a38
lingbopro <ilovemc@zohomail.cn>
2025-05-11 19:28:16 +0800

    🐞 fix: fix resource not found
    Set base dir to './' to prevent absolute dirs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+    base: './',
     build: {
         rollupOptions: {
             output: {
