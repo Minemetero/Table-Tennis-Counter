126787375842654a8e8896652888bc01fa5c10c5
Minemetero <minemetero@outlook.com>
2025-05-16 20:30:23 +0900

    ✨ feat: add confetti animation when player score

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import { lang } from './lang';
 import { hideLoading, showLoading, showSnackBar } from './ui';
+import confetti from 'canvas-confetti';
 
 let players = [];
 let totalScores = {};
@@ -146,6 +147,44 @@ export function incrementCurrentMatchScore(playerName) {
             )
         );
         disableScoreButtons();
+        
+        // Wining Animation
+        var count = 200;
+        var defaults = {
+            origin: { y: 0.7 }
+        };
+
+        function fire(particleRatio, opts) {
+            confetti({
+                ...defaults,
+                ...opts,
+                particleCount: Math.floor(count * particleRatio)
+            });
+        }
+
+        fire(0.25, {
+            spread: 26,
+            startVelocity: 55,
+        });
+        fire(0.2, {
+            spread: 60,
+        });
+        fire(0.35, {
+            spread: 100,
+            decay: 0.91,
+            scalar: 0.8
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 25,
+            decay: 0.92,
+            scalar: 1.2
+        });
+        fire(0.1, {
+            spread: 120,
+            startVelocity: 45,
+        });
+        
         setTimeout(() => {
             document.getElementById('result').innerText = '';
             updatePlayerScoreList();
