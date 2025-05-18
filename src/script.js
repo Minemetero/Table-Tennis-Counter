2a0a87225eb9b6f52fde177a26e8571d4aca1b7b
lingbopro <ilovemc@zohomail.cn>
2025-05-18 09:07:00 +0800

    🦄 refactor: move confetti functionality to UI module

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import { lang } from './lang';
-import { hideLoading, showLoading, showSnackBar } from './ui';
-import confetti from 'canvas-confetti';
+import { hideLoading, showLoading, showSnackBar, showWinnerConfetti } from './ui';
 
 let players = [];
 let totalScores = {};
@@ -149,41 +148,7 @@ export function incrementCurrentMatchScore(playerName) {
         disableScoreButtons();
         
         // Wining Animation
-        var count = 200;
-        var defaults = {
-            origin: { y: 0.7 }
-        };
-
-        function fire(particleRatio, opts) {
-            confetti({
-                ...defaults,
-                ...opts,
-                particleCount: Math.floor(count * particleRatio)
-            });
-        }
-
-        fire(0.25, {
-            spread: 26,
-            startVelocity: 55,
-        });
-        fire(0.2, {
-            spread: 60,
-        });
-        fire(0.35, {
-            spread: 100,
-            decay: 0.91,
-            scalar: 0.8
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 25,
-            decay: 0.92,
-            scalar: 1.2
-        });
-        fire(0.1, {
-            spread: 120,
-            startVelocity: 45,
-        });
+        showWinnerConfetti();
         
         setTimeout(() => {
             document.getElementById('result').innerText = '';
